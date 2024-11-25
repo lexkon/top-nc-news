900e9a1b83e0fad868cfa00b35c32c94b15e8b6d
Alex Kontis <alex@alexkontis.co.uk>
2024-11-25 12:02:51 +0000

    completes get/api/topics tests + endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,10 @@
-const endpointsJson = require("../endpoints.json");
+const endpointsJson = require("../endpoints.json")
 const request = require('supertest')
 const app = require('../app')
 const db = require('../db/connection')
 const seed = require('../db/seeds/seed')
 const data = require('../db/data/test-data')
+const topics = require("../db/data/test-data/topics")
 
 beforeEach(() => seed(data))
 afterAll(() => db.end())
@@ -14,7 +15,35 @@ describe("GET /api", () => {
       .get("/api")
       .expect(200)
       .then(({ body: { endpoints } }) => {
-        expect(endpoints).toEqual(endpointsJson);
-      });
-  });
-});
+        expect(endpoints).toEqual(endpointsJson)
+      })
+  })
+})
+
+describe("GET /api/topics", () => {
+  test("200: returns an array of topic objects each with properties of 'slug' and 'description'", () => {
+    return request(app)
+    .get('/api/topics')
+    .expect(200)
+    .then(() => {
+      expect(topics.length).toBe(3)
+      topics.forEach((topic) => {
+        expect(topic).toMatchObject({
+          slug: expect.any(String),
+          description: expect.any(String)
+        })
+      })
+    })
+  })
+})
+
+describe("Error handling", () => {
+  test("404: error when attempting to access a non-existent endpoint", () => {
+    return request(app)
+    .get("/api/banana")
+    .expect(404)
+    .then(({ body: { msg } }) => {
+      expect(msg).toBe('not an endpoint')
+    })
+  })
+})
--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
-const { getApi } = require('./controllers/app.controller')
+const { getApi, getTopics } = require('./controllers/app.controller')
 const app = express()
 
-app.use(express.json())
+// app.use(express.json())
 
 app.get('/api', getApi)
+
+app.get('/api/topics', getTopics)
+
+app.use((err, req, res, next) => {
+    console.error(err, "err inside app.js")
+    res.status(500).send({ msg: 'Something went wrong on the server' });
+})
+
+app.use('*', (_, res) => {
+    res.status(404).send({ msg: 'not an endpoint' });
+})
 
 module.exports = app
