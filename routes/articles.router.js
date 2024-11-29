096764cf42825e2a4bc13160897b29564481b565
Alex Kontis <alex@alexkontis.co.uk>
2024-11-29 13:52:23 +0000

    implements express routers

---
+++ routes/articles.router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { getArticles, getArticleById, getArticleComments, postComment, patchArticle } = require('../controllers');
+
+const articlesRouter = express.Router();
+
+articlesRouter.get('/', getArticles);
+articlesRouter.get('/:article_id', getArticleById);
+articlesRouter.get('/:article_id/comments', getArticleComments);
+articlesRouter.post('/:article_id/comments', postComment);
+articlesRouter.patch('/:article_id', patchArticle);
+
+module.exports = articlesRouter;
